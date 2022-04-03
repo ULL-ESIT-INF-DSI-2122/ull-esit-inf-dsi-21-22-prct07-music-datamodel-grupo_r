c3fb3010389ac7cf3b225ed36ac6583dc714a75d
Jonay <alu0101347141@ull.edu.es>
2022-04-03 21:39:42 +0100

    Implementadas clase Album y Group

---
+++ src/Album.ts
@@ -0,0 +1,121 @@
+import { Group } from './Group';
+import { Genre } from './Genres';
+import { Artist } from './Artist';
+import { Song } from './Song';
+
+/**
+ * @class Represents an album.
+ */
+export class Album {
+  /**
+   * Initialize an Album object.
+   * @param {string} title Album title
+   * @param {Artist | Group} author Artist or group that publish the album
+   * @param {string} date Release date
+   * @param {Genre[]} genres Related musical genres
+   * @param {Song[]} songs Songs list
+   */
+  constructor(
+    private title: string,
+    private author: Artist | Group,
+    private date: string,
+    private genres: Genre[],
+    private songs: Song[],
+  ) {
+    this.title = title;
+    this.author = author;
+    this.date = date;
+    this.genres = genres;
+    this.songs = songs;
+  }
+
+  /**
+   * Get the album title.
+   * @return {string} album title
+   */
+  public getTitle(): string {
+    return this.title;
+  }
+
+  /**
+   * Set the album title.
+   * @param {string} title Album title
+   */
+  public setTitle(title: string): void {
+    this.title = title;
+  }
+
+  /**
+   * Get the album author
+   * @return {Artist | Group} Author
+   */
+  public getAuthor(): Artist | Group {
+    return this.author;
+  }
+
+  /**
+   * Set the album author
+   * @param {Artist | Group} author Author
+   */
+  public setAuthor(author: Artist | Group): void {
+    this.author = author;
+  }
+
+  /**
+   * Get the release date
+   * @return {string} date
+   */
+   public getDate(): string {
+    return this.date;
+  }
+
+  /**
+   * Set the release date
+   * @param {string} date Release date
+   */
+  public setDate(date: string): void {
+    this.date = date;
+  }
+
+  /**
+   * Get the related musical genres's.
+   * @return {Genre[]} Genres
+   */
+  public getGenres(): Genre[] {
+    return this.genres;
+  }
+
+  /**
+   * Set the related musical genres.
+   * @param {Genres[]} genres Relate genres
+   */
+  public setGenres(genres: Genre[]): void {
+    this.genres = genres;
+  }
+
+  /**
+ * Add related musical genre(s).
+ * @param {Genres[]} genres Relate genre(s)
+ */
+    public addGenres(genres: Genre[]): void {
+      for (let i: number = 0; i < genres.length; i++) {
+        this.genres.push(genres[i]);
+      }
+  }
+
+  /**
+   * Get the songs list
+   * @return {Song[]} Songs
+   */
+  public getSongs(): Song[] {
+    return this.songs;
+  }
+
+  /**
+   * Set the songs list
+   * @param {Song[]} songs Songs list
+   */
+  public setSongs(songs: Song[]) {
+    this.songs = songs;
+  }
+}
---
+++ src/Group.ts
@@ -0,0 +1,139 @@
+import { Album } from './Album';
+import { Genre } from './Genres';
+import { Artist } from './Artist';
+import { Song } from './Song';
+
+/**
+ * @class Represents a group.
+ */
+export class Group {
+  /**
+   * Initialize a Group object.
+   * @param {string} name Album name
+   * @param {Artist[]} members Artists (group members)
+   * @param {string} date Creation date
+   * @param {Genre[]} genres Related musical genres
+   * @param {Album[]} albums Released albums
+   * @param {number} listeners Number of listeners
+   */
+  constructor(
+    private name: string,
+    private members: Artist[],
+    private date: string,
+    private genres: Genre[],
+    private albums: Album[],
+    private listeners: number
+  ) {
+    this.name = name;
+    this.members = members;
+    this.date = date;
+    this.genres = genres;
+    this.albums = albums;
+    this.listeners = listeners;
+  }
+
+  /**
+   * Get the group name.
+   * @return {string} group name
+   */
+  public getName(): string {
+    return this.name;
+  }
+
+  /**
+   * Set the group name.
+   * @param {string} name Group name
+   */
+  public setName(name: string): void {
+    this.name = name;
+  }
+
+  /**
+   * Get the group members
+   * @return {Artist[]} Members
+   */
+  public getMembers(): Artist[] {
+    return this.members;
+  }
+
+  /**
+   * Set the group author
+   * @param {Artist[]} members Group members
+   */
+  public setMembers(members: Artist[]): void {
+    this.members = members;
+  }
+
+  /**
+   * Get the creation date
+   * @return {string} date
+   */
+   public getDate(): string {
+    return this.date;
+  }
+
+  /**
+   * Set the creation date
+   * @param {string} date Creation date
+   */
+  public setDate(date: string): void {
+    this.date = date;
+  }
+
+  /**
+   * Get the related musical genres's.
+   * @return {Genre[]} Genres
+   */
+  public getGenres(): Genre[] {
+    return this.genres;
+  }
+
+  /**
+   * Set the related musical genres.
+   * @param {Genres[]} genres Relate genres
+   */
+  public setGenres(genres: Genre[]): void {
+    this.genres = genres;
+  }
+
+  /**
+ * Add related musical genre(s).
+ * @param {Genres[]} genres Relate genre(s)
+ */
+    public addGenres(genres: Genre[]): void {
+      for (let i: number = 0; i < genres.length; i++) {
+        this.genres.push(genres[i]);
+      }
+  }
+
+  /**
+   * Get the albums list
+   * @return {Album[]} Albums
+   */
+  public getAlbums(): Album[] {
+    return this.albums;
+  }
+
+  /**
+   * Set the albums list
+   * @param {Album[]} albums Albums list
+   */
+  public setAlbums(albums: Album[]) {
+    this.albums = albums;
+  }
+
+  /**
+   * 
+   * @return {number} Listeners
+   */
+  public getListeners(): number {
+    return this.listeners;
+  }
+
+  /**
+   * Increment listeners counter 
+   */
+  public incrementListeners(): void {
+    this.listeners++;
+  }
+}
